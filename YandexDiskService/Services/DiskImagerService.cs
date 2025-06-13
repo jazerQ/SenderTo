3d5d0756ce7d1a390d76aa073bd1adbcf2d061fc
JazerQ <saltanhayrliev2006@gmail.com>
2025-06-13 15:43:50 +0300

    добавил метод скачивания фотографий в микросервис диска, этот метод мне поможет для наложения ватермарки

--- YandexDiskService/Services/DiskImagerService.cs
+++ YandexDiskService/Services/DiskImagerService.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http.Headers;
 using System.Text.Json;
+using Google.Protobuf;
 using Grpc.Core;
 using GrpcDiskServiceApp;
 using Microsoft.Extensions.Options;
@@ -12,6 +13,8 @@ public class DiskImagerService : DiskImager.DiskImagerBase
     private readonly HttpClient _client;
     private readonly string _uploadedUrl =
         "https://cloud-api.yandex.net/v1/disk/resources/upload?path=disk:/SenderToPhotos/";
+    private readonly string _downloadedUrl =
+        "https://cloud-api.yandex.net/v1/disk/resources/download?path=disk:/SenderToPhotos/";
 
     public DiskImagerService(IHttpClientFactory httpClientFactory, IOptionsMonitor<YandexSettings> options)
     {
@@ -23,11 +26,8 @@ public class DiskImagerService : DiskImager.DiskImagerBase
     public override async Task<ImageUploadResponse> ImageUpload(ImageUploadRequest request, ServerCallContext context)
     {
         var filename = $"{Guid.NewGuid().ToString()}.png";
+        var uploadedLink = await GetLink(_uploadedUrl + filename);
         
-        var response = await _client.GetAsync(_uploadedUrl + filename);
-        var json = await response.Content.ReadAsStringAsync();
-        var uploadedLink = JsonDocument.Parse(json).RootElement.GetProperty("href").GetString();
-
         if (uploadedLink is null)
             throw new RpcException(Status.DefaultCancelled, "не смог загрузить фотографию, не удалось получить ссылку для загрузки");
 
@@ -41,5 +41,32 @@ public class DiskImagerService : DiskImager.DiskImagerBase
         {
             Filename = filename
         };
+    }
+
+    public override async Task<ImageDownloadResponse> ImageDownload(ImageDownloadRequest request, ServerCallContext context)
+    {
+        var downloadedUrl = await GetLink(_downloadedUrl + request.Filename);
+        if (downloadedUrl is null)
+            throw new RpcException( Status.DefaultCancelled, "не смог загрузить фотографию, не удалось получить ссылку для загрузки");
+
+        var fileBytesResponseMessage = await _client.GetAsync(downloadedUrl);
+        if(fileBytesResponseMessage.StatusCode != HttpStatusCode.OK)
+            throw new RpcException(Status.DefaultCancelled, "не смог загрузить фотографию");
+
+        var fileBytes = await fileBytesResponseMessage.Content.ReadAsByteArrayAsync();
+
+        return new ImageDownloadResponse()
+        {
+            Image = ByteString.CopyFrom(fileBytes)
+        };
+    }
+    
+    private async Task<string?> GetLink(string url)
+    {
+        var response = await _client.GetAsync(url);
+        var json = await response.Content.ReadAsStringAsync();
+        var link = JsonDocument.Parse(json).RootElement.GetProperty("href").GetString();
+
+        return link;
     }
 }
