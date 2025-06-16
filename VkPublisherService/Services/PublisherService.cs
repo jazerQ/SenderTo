7e21fad4921db992e4b9219c16eb2bc2d47ad1b5
JazerQ <saltanhayrliev2006@gmail.com>
2025-06-16 09:05:09 +0300

    добавил протобаф для вк паблишера

--- VkPublisherService/Services/PublisherService.cs
+++ VkPublisherService/Services/PublisherService.cs
@@ -9,6 +9,7 @@ public class PublisherService(IHttpClientFactory factory, IOptionsMonitor<VkSett
 {
     private readonly HttpClient _client = factory.CreateClient();
     private readonly string _postUrl = "https://api.vk.com/method/wall.post";
+    private readonly string _uploadPhoto = "https://api.vk.com/method/photos.getWallUploadServer";
     
     public override async Task<CreatePostResponse> CreatePost(CreatePostRequest request, ServerCallContext context)
     {
@@ -23,5 +24,13 @@ public class PublisherService(IHttpClientFactory factory, IOptionsMonitor<VkSett
         var content = new FormUrlEncodedContent(parameters);
 
         var response = await _client.PostAsync(_postUrl, content);
+    }
+
+    private string getUrl()
+    {
+        HttpRequestMessage request = new HttpRequestMessage();
+        curl 'https://api.vk.com/method/photos.getWallUploadServer' \
+        -F 'access_token=vk1.a.i1ds9XIqwke3EoxhHwifUi4_UVy-KTj9GP4C_jqmBYa-a7SSVpmbFN2OoJQar6Ql75KqX9zqnTe7ffbh9sg3u7WHQwAPxTSb2lD5DOv1GXdI5lT1S4xkljD_WfYa5kHi_LNq6Z7wTVDHU0xMy_WMrE2AJ9TpZsuOdPhNUcnsjG1NPJgC_TZWrMLoMSKEya7ASNxmjrb7EVnqUP8mhYdfMA' \
+        -F 'v=5.131'
     }
 }
